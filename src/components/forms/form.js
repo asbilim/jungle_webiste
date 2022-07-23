69c011734f00009ce7a0ec210f23292988df8195
asbilim <bilimdepaullilian@gmail.com>
2022-07-23 22:45:06 +0200

    pushing

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
+
+function getName((e)=>{
+    name = setNameValue(e => e.target.value);
+    console.log(name);
+})
+
 function handleForm(e){
     e.preventDefault();
-    setNameValue(e.target.value)
 }
+
 function Form(){
     const [nameValue,setNameValue] = useState(initialState)
     return(
-        <form onSubmit={(e)=>{handleForm(e)}} method="post">
+        <form onSubmit={(e)=>{handleForm(e)}}  onClick={(e)=>getName(e)} method="post">
             <input type="text" name="name" placeholder="enter your name"/>
             <input type="submit" name="submit" value="Submit"/>
         </form>
